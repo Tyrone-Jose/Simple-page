dd6dc5d2c1241453d1bf95849e41ac385c874635
Alexander Pereira <josexdgamer15@gmail.com>
2020-11-17 04:57:42 -0430

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Simple page using 
+
+Html,js,css
---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+	//code By Null-Crypto
+
+	$(document).ready(function(){
+	
+		$('#open_menu').click(function(event) {
+			/* Act on the event */
+		$('nav ul').css({
+				visibility: 'visible',
+				opacity: '1'
+		});
+
+		});
+
+});//end jquery
